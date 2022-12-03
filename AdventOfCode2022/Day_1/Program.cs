a842faa39cf62042810bdc4967d0d046128686ec
b_bozyczko <b.bartosz_ext@aviolinx.com>
2022-12-03 19:57:26 +0100

    day 1 and 2 of advent of code

---
+++ AdventOfCode2022/Day_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Part 1
+/*
+var result = File.ReadAllText("input.txt")
+    .Split("\n\n")
+    .Select(group => group.Split("\n")
+        .Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse))
+    .Select(x => x.Sum())
+    .Max();
+*/
+//Part 2  
+
+var result = File.ReadAllText("input.txt")
+    .Split("\n\n")
+    .Select(group => group.Split("\n")
+        .Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse))
+    .Select(x => x.Sum())
+    .OrderByDescending(x => x)
+    .Take(3)
+    .Sum();
+
+Console.WriteLine(result);
---
+++ AdventOfCode2022/Day_2/Program.cs
@@ -0,0 +1,79 @@
+﻿var file = File.ReadAllText("input.txt");
+var lines = file.Split("\n");
+lines = lines.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+var score = 0;
+
+// A = rock
+// B = paper
+// C = scissors
+
+// X = lose
+// Y = draw
+// Z = win
+
+var scoreDict = new Dictionary<string, int>()
+{
+    {"A", 1},
+    {"B", 2},
+    {"C", 3}
+};
+
+foreach (var line in lines)
+{
+    var rps = line.Split(" ");
+    var opponent = rps[0];
+    var me = rps[1];
+
+    switch (me)
+    {
+        case "X":
+            score += 0;
+            score += scoreDict[GetLoser(opponent)];
+            break;
+        case "Y":
+            score += 3;
+            score += scoreDict[opponent];
+            break;
+        case "Z":
+            score += 6;
+            score += scoreDict[GetWinner(opponent)];
+            break;
+    }
+}
+
+Console.WriteLine(score);
+
+Console.ReadKey();
+
+string GetWinner(string move)
+{
+    switch (move)
+    {
+        case "A":
+            return "B";
+        case "B":
+            return "C";
+        case "C":
+            return "A";
+    }
+
+    return "";
+}
+
+string GetLoser(string move)
+{
+    switch (move)
+    {
+        case "A":
+            return "C";
+        case "B":
+            return "A";
+        case "C":
+            return "B";
+    }
+
+    return "";
+}
+
+//Part 1 is missing cuz of me forgetting to commit lol
